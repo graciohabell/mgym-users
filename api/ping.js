b98bd2382d61397e4a8075ee4d280568457ecc57
gracio habel <graciohabelll@gmail.com>
2025-09-30 08:31:04 +0700

    api fix

--- api/ping.js
+++ api/ping.js
@@ -1,14 +1,21 @@
 export default async function handler(req, res) {
     try {
-      const r = await fetch("https://<MGYM-USERS>.supabase.co/rest/v1/APIAPIAPI?limit=1", {
+      const r = await fetch("https://iilbqhjwoyakuvckkqfx.supabase.co/rest/v1/APIAPIAPI?id=eq.1", {
+        method: "PATCH",
         headers: {
           apikey: process.env.SUPABASE_ANON_KEY,
           Authorization: `Bearer ${process.env.SUPABASE_ANON_KEY}`,
+          "Content-Type": "application/json",
+          Prefer: "return=representation", 
         },
+        body: JSON.stringify({
+          dummy: Date.now(), 
+        }),
       });
   
-      
-      res.status(200).json({ ok: true, status: r.status });
+      const data = await r.json();
+  
+      res.status(200).json({ ok: true, status: r.status, data });
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
